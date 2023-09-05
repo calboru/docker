41ace6823413b38cab04fd5edf2c49ec2b377da5
Hayri Calboru <hcalboru@ecri.org>
2023-09-04 21:30:21 -0400

    .

--- secure-ssh-server/ssh-server/start_ssh_server.sh
+++ secure-ssh-server/ssh-server/start_ssh_server.sh
@@ -4,22 +4,20 @@ path="/etc/ssh"
 fifoFile="$path/ssh_fifo"
 logpath=/var/log
 
-/usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /var/log/auth.log
+#/usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /var/log/auth.log
 
 #/usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /etc/ssh/ssh_fifo
 
+## Check if pipe exists or fail
+if [[ ! -p $fifoFile ]];then
+   mkfifo $fifoFile
+   [[ ! -p $fifoFile ]] && echo "ERROR: Failed to create FIFO file" && exit 1
+fi
 
-# ## Check if pipe exists or fail
-# if [[ ! -p $fifoFile ]];then
-#    mkfifo $fifoFile
-#    [[ ! -p $fifoFile ]] && echo "ERROR: Failed to create FIFO file" && exit 1
-# fi
-
-# ## Monitor the FIFO file and store the SSHD logs
-# while true
-# do
-#     if read line; then
-#        printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line" >> "$logpath/sshd_with_timestamp.log"
-#     fi
-# done <"$fifoFile"
-
+## Monitor the FIFO file and store the SSHD logs
+while true
+do
+    if read line; then
+       printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line" >> "$path/sshd_debug.log"
+    fi
+done <"$fifoFile"
