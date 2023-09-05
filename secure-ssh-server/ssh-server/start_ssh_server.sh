9ec81c0bcb1ab8af6130e813946847e20c46c880
Hayri Calboru <hcalboru@ecri.org>
2023-09-04 21:45:17 -0400

    .

--- secure-ssh-server/ssh-server/start_ssh_server.sh
+++ secure-ssh-server/ssh-server/start_ssh_server.sh
@@ -4,25 +4,25 @@ path="/etc/ssh"
 fifoFile="$path/ssh_fifo"
 logpath=/var/log
 
-#/usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /var/log/auth.log
+/usr/sbin/sshd -D  -ddd 2>&1  | while read line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; 
 
-echo 'RUNNING SSHD SERVER'
-/usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /etc/ssh/ssh_fifo
+# echo 'RUNNING SSHD SERVER'
+# /usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /etc/ssh/ssh_fifo
 
-echo 'EXECUTING THE FIFO LOOP'
+# echo 'EXECUTING THE FIFO LOOP'
 
-## Check if pipe exists or fail
-if [[ ! -p $fifoFile ]];then
-   echo 'CREATING FIFO FILE BECAUSE NOT FOUND'
-   mkfifo $fifoFile
-   [[ ! -p $fifoFile ]] && echo "ERROR: Failed to create FIFO file" && exit 1
-fi
+# ## Check if pipe exists or fail
+# if [[ ! -p $fifoFile ]];then
+#    echo 'CREATING FIFO FILE BECAUSE NOT FOUND'
+#    mkfifo $fifoFile
+#    [[ ! -p $fifoFile ]] && echo "ERROR: Failed to create FIFO file" && exit 1
+# fi
 
-## Monitor the FIFO file and store the SSHD logs
-while true
-do
-    if read line; then
-       echo '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"
-       printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line" >> "$path/sshd_debug.log"
-    fi
-done <"$fifoFile"
+# ## Monitor the FIFO file and store the SSHD logs
+# while true
+# do
+#     if read line; then
+#        echo '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"
+#        printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line" >> "$path/sshd_debug.log"
+#     fi
+# done <"$fifoFile"
