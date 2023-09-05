e4c77f19f73212477e688f9c8c838d9690a0415e
Hayri Calboru <hcalboru@ecri.org>
2023-09-05 11:18:40 -0400

    .

--- secure-ssh-server/ssh-server/start_ssh_server.sh
+++ secure-ssh-server/ssh-server/start_ssh_server.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-/usr/sbin/sshd -D -E /var/log/auth.log
+
+/usr/sbin/sshd -D -E /var/log/auth.log &
 
 /bin/crowdsec-firewall-bouncer -v -c /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
