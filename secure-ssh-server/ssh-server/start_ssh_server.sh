a955d3f311f179372b65da7f0c73c3e93e6e58ed
Hayri Calboru <hcalboru@ecri.org>
2023-09-05 10:47:23 -0400

    .

--- secure-ssh-server/ssh-server/start_ssh_server.sh
+++ secure-ssh-server/ssh-server/start_ssh_server.sh
@@ -6,25 +6,4 @@ logpath=/var/log
 
 /usr/sbin/sshd -D -E /var/log/auth.log
 
-# #/usr/sbin/sshd -D  2>&1 | while read line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | tee -a /var/log/auth_with_timestamp.log
-
-# echo 'RUNNING SSHD SERVER'
-# /usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /tmp/ssh_fifo
-
-# echo 'EXECUTING THE FIFO LOOP'
-
-# ## Check if pipe exists or fail
-# if [[ ! -p $fifoFile ]];then
-#    echo 'CREATING FIFO FILE BECAUSE NOT FOUND'
-#    mkfifo $fifoFile
-#    [[ ! -p $fifoFile ]] && echo "ERROR: Failed to create FIFO file" && exit 1
-# fi
-
-# ## Monitor the FIFO file and store the SSHD logs
-# while true
-# do
-#     if read line; then
-#        echo '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"
-#        printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line" >> "$logpath/sshd_debug.log"
-#     fi
-# done <"$fifoFile"
+/bin/crowdsec-firewall-bouncer -v -c /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
