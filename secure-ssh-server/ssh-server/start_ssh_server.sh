85c7fea471b601255e2822fd98deaa9113495dd0
Hayri Calboru <hcalboru@ecri.org>
2023-09-04 22:08:09 -0400

    .

--- secure-ssh-server/ssh-server/start_ssh_server.sh
+++ secure-ssh-server/ssh-server/start_ssh_server.sh
@@ -4,7 +4,7 @@ path="/etc/ssh"
 fifoFile="$path/ssh_fifo"
 logpath=/var/log
 
-/usr/sbin/sshd -D  2>&1 -dd | while read line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | tee -a /var/log/auth_with_timestamp.log
+/usr/sbin/sshd -D  2>&1 -d | while read line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | tee -a /var/log/auth_with_timestamp.log
 
 # echo 'RUNNING SSHD SERVER'
 # /usr/sbin/sshd -D -f /etc/ssh/sshd_config -E /etc/ssh/ssh_fifo
